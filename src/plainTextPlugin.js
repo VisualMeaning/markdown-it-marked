07bcd5a89a2be740535b575f9ddc96cbb1492db8
Mingya Zhou <mingya.zhou@visual-meaning.com>
2024-02-27 14:34:10 +0000

    first pass create plainTextPlugin.js

---
+++ src/plainTextPlugin.js
@@ -0,0 +1,42 @@
+/*
+ * Plain text extractor for markdown-it.
+ *
+ * https://github.com/wavesheep/markdown-it-plain-text
+ */
+
+export function plainTextPlugin(md) {
+
+  const scan = (tokens) => {
+    let text = '';
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+      if (token.children !== null) {
+        text += scan(token.children);
+      } else {
+        if (
+          token.type === 'text' ||
+          token.type === 'fence' ||
+          token.type === 'html_block' ||
+          token.type === 'code_block' ||
+          token.type === 'code_inline' ||
+          token.type === 'html_inline' ||
+          token.type === 'emoji'
+        ) {
+          text += token.content;
+        } else if (/[a-zA-Z]+_close/.test(token.type)) { // prevent words from sticking together
+          text += " "
+        }
+      }
+    }
+
+    return text;
+  }
+
+  const rule = function plainTextRule(state) {
+    const text = scan(state.tokens);
+    // remove redundant white spaces
+    md.plainText = text.replace(/\s+/g, " ");
+  };
+
+  md.core.ruler.push('plainText', plainTextRule);
+}
